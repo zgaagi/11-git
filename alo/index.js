bd4c0b5929e094244f1fc1b4e6f9907a025bbd22
zgaagi <67436797+zgaagi@users.noreply.github.com>
2020-08-06 22:03:43 +0300

    Add files via upload

---
+++ alo/index.js
@@ -0,0 +1,73 @@
+// let age = 1;
+// while (age < 30) {
+//     console.log(`Мне ${age} лет`);
+//     age++;
+// }
+
+// do while------------------
+// let age = 1;
+// do {
+//     colsole.log(`Мне ${age} лет`);
+//     age++;
+// }  while (age < 30);
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0 ) continue;
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0 ) break;
+//     console.log(i);
+// }
+
+
+
+// let a = 2 + 2;
+
+// switch (a) {
+//     case 3:
+//       alert("Маловато");
+//       break;
+//     case 4:
+//         alert("В точку!");
+//         break;
+//     case 5:
+//         alert("Перебор");
+//         break;
+//     default:
+//         alert("Нет таких значений");
+// }
+
+
+// let fruits = ["Яблоко", "Апельсин", "Слива"];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// console.log("length: ", fruits.length);
+
+
+
+//  let fruits = ["Яблоко", "Апельсин", "Слива"];
+// fruits.pop();
+// console.log(fruits);
+
+
+
+// let fruits = ["Яблоко", "Слива"];
+// fruits.push("Апельсин");
+// console.log(fruits); // Яблоко слива Апельсин
+
+
+let fruits = ["Яблоко", "Слива"];
+fruits.push("Апельсин");
+console.log(fruits); 
+
+
+
+
+
