b419eb754c5995f569744713bc3403dc5d71708e
zgaagi <67436797+zgaagi@users.noreply.github.com>
2020-08-17 21:59:05 +0300

    Add files via upload

---
+++ alo/function/index.js
@@ -0,0 +1,58 @@
+// function func(name) {
+//     console.log("function " + name);
+// }
+// func("nameFunction");
+
+// const getName = () => prompt("name");
+
+
+
+
+
+// function getname () {
+//     console.log = prompt("name");
+//     return name;
+// }
+
+const firstName = getName();
+console.log(firstName);
+
+
+
+
+// function sum(a,b) {
+//     console.log(a + b);
+// }
+// sum(2,7);
+
+
+
+// const name = prompt ("Ваше имя");
+// valid(name)
+
+// const surName = prompt ("Ваша фамилия");
+// valid(surName)
+
+// const lastName = prompt ("Ваше отчество");
+// valid(lastName)
+
+// function valid(value) {
+//     if(value === "") {
+//         console.log(false)
+//     }
+//     else {
+//         console.log(true)
+//     }
+// }
+
+
+
+function func () {
+    console.log("firstName " + name);
+}
+func(5);
+
+const arrowFunc = (name) => console.log("firstName " + name);
+arrowFunc(10); 
+
+
